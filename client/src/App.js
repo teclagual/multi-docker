839b64702ac99fb50917d9cda8c1dd04d9e31fd9
Gual Casas <gualcm@teclag.com>
2019-06-29 15:48:35 -0500

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import OtherPage from './OtherPage'
+import Fib from './Fib'
+
+function App() {
+   return (
+      <Router>
+         <div className="App">
+            <header>
+               <Link to="/">Home</Link>
+               <br />
+               <Link to="/otherpage">Other Page</Link>
+            </header>
+            <div>
+               <Route exact path="/" component={Fib} />
+               <Route path="/otherpage" component={OtherPage} />
+            </div>
+         </div>
+      </Router>
+   );
+}
+
+export default App;
